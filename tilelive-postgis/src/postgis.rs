49ffcf48d4b852e1e0ce0b4eee8a9b076b1174f2
Stepan Kuzmin <stepankuzmin@yandex-team.ru>
2019-10-08 17:51:10 +0300

    refactor: 💡 use std::io::Result for source functions

--- tilelive-postgis/src/postgis.rs
+++ tilelive-postgis/src/postgis.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::io;
 
 use r2d2::Pool;
 use r2d2_postgres::{PostgresConnectionManager, TlsMode};
@@ -14,32 +15,36 @@ static DEFAULT_EXTENT: u32 = 4096;
 static DEFAULT_BUFFER: u32 = 64;
 static DEFAULT_CLIP_GEOM: bool = true;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct PostGIS {
   pool: PostgresPool,
   query: String,
 }
 
 impl Source for PostGIS {
-  fn load(uri: &str) -> Result<Self, url::ParseError> {
-    let uri = url::Url::parse(uri)?;
+  fn load(uri: &str) -> io::Result<Self> {
+    let uri = url::Url::parse(uri)
+      .map_err(|err| io::Error::new(io::ErrorKind::Other, err.description()))?;
+
     let params: HashMap<_, _> = uri.query_pairs().collect();
 
     let mut uri = uri.clone();
     uri.set_query(None);
     let conn_str = uri.as_str();
 
-    let manager = PostgresConnectionManager::new(conn_str, TlsMode::None).unwrap();
-    let pool = Pool::builder().build(manager).unwrap();
+    let manager = PostgresConnectionManager::new(conn_str, TlsMode::None)?;
+
+    let pool = Pool::builder()
+      .build(manager)
+      .map_err(|err| io::Error::new(io::ErrorKind::Other, err.description()))?;
 
     let schema = params.get("schema").unwrap().to_string();
     let table = params.get("table").unwrap().to_string();
     let geometry_column = params.get("geometry_column").unwrap().to_string();
-    let id = format!("{}.{}", schema, table);
 
     let query = format!(
       include_str!("get_tile.sql"),
-      id = id,
+      id = format!("{}.{}", schema, table),
       geometry_column = geometry_column,
       extent = DEFAULT_EXTENT,
       buffer = DEFAULT_BUFFER,
@@ -49,18 +54,18 @@ impl Source for PostGIS {
     Ok(PostGIS { pool, query })
   }
 
-  fn get_tile(&self, z: u8, x: u32, y: u32) -> std::io::Result<Tile> {
+  fn get_tile(&self, z: u8, x: u32, y: u32) -> io::Result<Tile> {
     let conn = self
       .pool
       .get()
-      .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.description()))?;
+      .map_err(|err| io::Error::new(io::ErrorKind::Other, err.description()))?;
 
     let stmt = conn.prepare(&self.query)?;
 
-    let tile: Result<Tile, _> = stmt
+    let tile: io::Result<Tile> = stmt
       .query(&[&(z as i32), &(x as i32), &(y as i32)])
       .map(|rows| rows.get(0).get("st_asmvt"))
-      .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err.description()));
+      .map_err(|err| io::Error::new(io::ErrorKind::Other, err.description()));
 
     tile
   }
--- tilelive/src/source.rs
+++ tilelive/src/source.rs
@@ -1,11 +1,11 @@
-use url;
+use std::io::Result;
 
 use crate::tile::Tile;
 
 pub trait Source {
-  fn load(uri: &str) -> Result<Self, url::ParseError>
+  fn load(uri: &str) -> Result<Self>
   where
     Self: std::marker::Sized;
 
-  fn get_tile(&self, z: u8, x: u32, y: u32) -> std::io::Result<Tile>;
+  fn get_tile(&self, z: u8, x: u32, y: u32) -> Result<Tile>;
 }
